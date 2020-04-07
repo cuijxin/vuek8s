cc61f8c9ed96f3a470e4dbca1e9adc75839b1daa
cuijxin <cuijxin@126.com>
2020-04-07 09:27:25 +0000

    update

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+<el-form :rules="rules" class="login-container" label-position="left" label-width="0px" v-loading="loading">
+<h3 class="login_title">登录系统</h3>
+<el-form-item prop="account">
+    <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
+</el-form-item>
+<el-form-item prop="checkPass">
+    <el-input type="password" v-model="loginForm.password" auto-complete="off" placeolder="密码"></el-input>
+</el-form-item>
+<el-checkbox class="login_remember" v-model="checked" label-position="left">记住密码</el-checkbox>
+<el-form-item style="width: 100%">
+    <el-button type="primary" style="width: 100%" @click="submitClick">登录</el-button>
+</el-form-item>
+</el-form>
+</template>
--- src/utils/api.js
+++ src/utils/api.js
@@ -28,3 +28,55 @@ axios.interceptors.response.use(success => {
     }
     return;
 })
+
+let base = '';
+
+export const postKeyValueRequest = (url, params) => {
+    return axios({
+        method: 'post',
+        url: `${base}${url}`,
+        data: params,
+        transformRequest: [function (data){
+            let ret = '';
+            for (let i in data) {
+                ret += encodeURIComponent(i) + '=' + encodeURIComponent(data[i]) + '&'
+            }
+            return ret;
+        }],
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+}
+
+export const postRequest = (url, params) => {
+    return axios({
+        method: 'post',
+        url: `${base}${url}`,
+        data: params
+    })
+}
+
+export const putRequest = (url, params) => {
+    return axios({
+        method: 'put',
+        url: `${base}${url}`,
+        data: params
+    })
+}
+
+export const getRequest = (url, params) => {
+    return axios({
+        method: 'get',
+        url: `${base}${url}`,
+        params: params
+    })
+}
+
+export const deleteRequest = (url, params) => {
+    return axios({
+        method: 'delete',
+        url: `${base}${url}`,
+        params: params
+    })
+}
